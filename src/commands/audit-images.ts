fdaca8f596e5831505a3d92a49433973d64e1067
Loïs <58104051+loyfael@users.noreply.github.com>
2025-04-10 11:47:46 +0200

    feat: update

--- src/commands/audit-images.ts
+++ src/commands/audit-images.ts
@@ -35,7 +35,7 @@ function askQuestion(query: string): Promise<string> {
  */
 function fileExistsLocally(filePath: string): boolean {
   const uploadsRoot = process.env.LOCAL_UPLOADS_PATH
-  console.log(`Your uploads path is: ${uploadsRoot}`)
+  
   if (!uploadsRoot) {
     throw new Error('❌ LOCAL_UPLOADS_PATH is not defined in your .env file.')
   }
@@ -64,6 +64,7 @@ async function fileExistsOnS3(key: string): Promise<boolean> {
  */
 export async function auditImagesCommand() {
   const uploadsRoot = process.env.LOCAL_UPLOADS_PATH
+  console.log(`Your uploads path is: ${uploadsRoot}`)
 
   // 🔒 Validate uploads path at the very beginning
   if (!uploadsRoot) {
